256bf09ed61f2d68cd92af722a3ee9448a378d75
Ryan Liu <ryan@Ryans-MacBook-Pro.local>
2018-04-22 20:50:18 +1000

    updated carrierwave.rb file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,19 @@ CarrierWave.configure do |config|
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = ENV["FOG_DIRECTORY"]
-  config.storage = :fog                             # required
+
+  # For testing, upload files to local `tmp` folder.
+ if Rails.env.test? || Rails.env.cucumber?
+   config.storage = :file
+   config.enable_processing = false
+   config.root = "#{Rails.root}/tmp"
+ else
+   config.storage = :fog
+ end
+
+ config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+
+ config.fog_directory    = ENV["FOG_DIRECTORY"]
   #config.fog_public     = false                                                 # optional, defaults to true
   #config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
